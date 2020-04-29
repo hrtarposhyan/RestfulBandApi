bf2296d9cac55c246ea3a86dc5fc6b0255b2f46b
hrtarposhyan <hrtarposhyan@gmail.com>
2020-04-29 18:13:06 +0400

    Adding DBContext

---
+++ BandAPI/BandAPI/DBContext/LibraryContext.cs
@@ -0,0 +1,19 @@
+﻿using BandAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandAPI.DBContext
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Band> Bands { get; set; }
+        public DbSet<Album> Albums { get; set; }
+    }
+}
