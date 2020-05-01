e9043966c95929d155f88ce239b0747f9382860a
hrtarposhyan <hrtarposhyan@gmail.com>
2020-05-01 19:39:48 +0400

    Creating Collection of Resource Adding Support for OPTIONS Content Type Settings

---
+++ BandAPI/BandAPI/Controllers/BandCollectionsController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using BandAPI.Models;
+using BandAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandAPI.Controllers
+{
+    [ApiController]
+    [Route("api/bandcollections")]
+    public class BandCollectionsController:ControllerBase
+    {
+        private readonly ILibraryrepository _libraryrepository;
+        private readonly IMapper _mapper;
+
+        public BandCollectionsController(ILibraryrepository libraryrepository, IMapper mapper)
+        {
+            _libraryrepository = libraryrepository ??
+                throw new ArgumentNullException(nameof(libraryrepository));
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<BandDto>> CreateBandCollection([FromBody]
+           IEnumerable<BandForCreatingDto> bandCollection)
+        {
+            var bandEntities = _mapper.Map<IEnumerable<Entities.Band>>(bandCollection);
+            foreach (var band in bandEntities)
+            {
+                _libraryrepository.AddBand(band);
+            }
+            _libraryrepository.Save();
+            return Ok();
+        }
+    }
+}
--- BandAPI/BandAPI/Controllers/BandsController.cs
+++ BandAPI/BandAPI/Controllers/BandsController.cs
@@ -75,5 +75,12 @@ namespace BandAPI.Controllers
             return CreatedAtRoute("GetBand", new { bandId = bandToReturn.Id }, 
                 bandToReturn);
         }
+
+        [HttpOptions]
+        public IActionResult GetBandsOptions()
+        {
+            Response.Headers.Add("Allow", "GET,POST,DELETE,HEAD,OPTIONS");
+            return Ok();
+        }
     }
 }
